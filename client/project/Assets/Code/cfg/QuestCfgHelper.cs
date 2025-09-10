2709177e2442b9cc802bd3f5464af44bc56a85ef
fish-tennis <93663886@qq.com>
2025-09-10 16:31:28 +0800

    配置数据筛选接口

--- client/project/Assets/Code/cfg/QuestCfgHelper.cs
+++ client/project/Assets/Code/cfg/QuestCfgHelper.cs
@@ -9,11 +9,14 @@ namespace cshap_client.cfg
     {
         public static Dictionary<int,QuestDict> QuestsByQuestType; // 任务类型索引
         public static Dictionary<int,QuestDict> QuestsByCategory; // 任务分类索引
+        public static List<Gserver.QuestCfg> Achievement; // 成就任务
         
         public static void AfterLoad()
         {
             QuestsByQuestType = Helper.CreateDictionaryIndex(DataMgr.Quests, (questCfg) => questCfg.QuestType);
             QuestsByCategory = Helper.CreateDictionaryIndex(DataMgr.Quests, (questCfg) => questCfg.Category);
+            Achievement = Helper.CreateList(DataMgr.Quests,  (questCfg) => questCfg.QuestType == (int)Gserver.QuestType.Achievement, null);
+            Achievement.Sort((a, b) => a.CfgId.CompareTo(b.CfgId));
         }
     }
 }
